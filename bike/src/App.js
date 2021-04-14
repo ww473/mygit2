c8d53b4fdbe134546af1e26e3940a7576fa291b6
zjw <zjw88882021@163.com>
2021-04-14 09:35:26 +0800

    bike

---
+++ bike/src/App.js
@@ -0,0 +1,11 @@
+import {HashRouter as Router,useHistory} from 'react-router-dom'
+
+import RouterView from './router'
+function App(){
+  
+
+  return <Router>
+    <RouterView></RouterView>
+  </Router>
+}
+export default App;
